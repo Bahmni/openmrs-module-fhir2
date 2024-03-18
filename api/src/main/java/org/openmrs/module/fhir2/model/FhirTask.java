867f26ccd212cea0c1ea5e0992f7105735645d18
Kalaiyarasan Raja <kraja@investvoyager.com>
2024-03-18 09:54:11 +0530

    Changes to Fhir task model and its translator changes

--- api/src/main/java/org/openmrs/module/fhir2/model/FhirTask.java
+++ api/src/main/java/org/openmrs/module/fhir2/model/FhirTask.java
@@ -9,26 +9,16 @@
  */
 package org.openmrs.module.fhir2.model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
+import java.util.Date;
 import java.util.Set;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.openmrs.BaseOpenmrsMetadata;
+import org.openmrs.Concept;
 
 @Data
 @NoArgsConstructor
@@ -129,4 +119,31 @@ public class FhirTask extends BaseOpenmrsMetadata {
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "location_reference_id", referencedColumnName = "reference_id")
 	private FhirReference locationReference;
+	
+	/**
+	 * Type of Task
+	 */
+	@ManyToOne
+	@JoinColumn(name = "task_code", referencedColumnName = "concept_id")
+	protected Concept taskCode;
+	
+	/**
+	 * Task that this particular task is part of.
+	 */
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "fhir_task_part_of_reference", joinColumns = @JoinColumn(name = "task_id"), inverseJoinColumns = @JoinColumn(name = "reference_id"))
+	private Set<FhirReference> partOfReferences;
+	
+	/**
+	 * Actual start time of the execution
+	 */
+	@Column(name = "execution_start_time")
+	private Date executionStartTime;
+	
+	/**
+	 * Actual end time of the execution
+	 */
+	@Column(name = "execution_end_time")
+	private Date executionEndTime;
+	
 }
