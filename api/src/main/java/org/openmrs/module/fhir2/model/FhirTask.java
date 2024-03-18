0b81ae117b3191cb009bface54face242fb8db0f
Kalaiyarasan Raja <kraja@investvoyager.com>
2024-03-18 13:10:54 +0530

    Replaced Import as a whole with specific classes

--- api/src/main/java/org/openmrs/module/fhir2/model/FhirTask.java
+++ api/src/main/java/org/openmrs/module/fhir2/model/FhirTask.java
@@ -9,7 +9,20 @@
  */
 package org.openmrs.module.fhir2.model;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import java.util.Date;
 import java.util.Set;
